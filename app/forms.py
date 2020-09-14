89e1d02e6dc336bbd67b62610496ee964a967940
ljubimova olesja <lyubimova.on@phystech.edu>
2020-09-14 18:00:54 +0300

    front

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
-
+from wtforms.validators import DataRequired, ValidationError
+from app.models import User
+from flask_login import current_user
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -11,3 +12,17 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Register')
+
+class SearchUserForm(FlaskForm):
+    username = StringField('Search user', validators=[DataRequired()])
+    submit = SubmitField('Search')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is None:
+            raise ValidationError('user not found')
+        elif user == User.query.get(int(current_user.get_id())):
+            raise ValidationError("can't create a chat with yourself")
+
+class SendMessageForm(FlaskForm):
+    message = StringField('Send message', validators=[DataRequired()])
+    submit = SubmitField('Send')
--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,56 @@
-from flask import render_template, redirect, flash, url_for
-from app.forms import LoginForm, RegistrationForm
+from flask import render_template, redirect, flash, url_for, request
+from app.forms import LoginForm, RegistrationForm, SearchUserForm, SendMessageForm
 from app import app
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Chat, Message
+from app import db
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/dialogues')
+@app.route('/dialogues', methods=['GET','POST'])
 @login_required
 def dialogues():
-    return render_template('dialogues.html')
+    user = User.query.get(int(current_user.get_id()))
+    chats = user.chats
+    contacts = []
+    for chat in chats:
+        for contact in chat.users:
+            if contact.username != user.username:
+                contacts.append({"username" : contact.username, "chat" : chat.id})
+    form = SearchUserForm()
+    if form.validate_on_submit():
+        contact = User.query.filter_by(username=form.username.data).first()
+        for chat in chats:
+            for user in chat.users:
+                if user.username == contact.username:
+                    return redirect("{}?chat={}".format(url_for('messages'), chat.id))
+        new_chat = Chat(users=[user, contact])
+        db.session.add(new_chat)
+        db.session.commit()
+        return redirect("{}?chat={}".format(url_for('messages'), new_chat.id))
+    return render_template('dialogues.html', contacts=contacts, form=form)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 @login_required
 def messages():
-    return render_template('messages.html')
+    try:
+        chat_id = int(request.args.get('chat'))
+        chat = Chat.query.get(chat_id)
+        form = SendMessageForm()
+        if form.validate_on_submit():
+            user = User.query.get(int(current_user.get_id()))
+            message = Message(message=form.message.data, sent_from=user, chat=chat)
+            db.session.add(message)
+            db.session.commit()
+        messages = []
+        for message in chat.messages:
+            messages.append({'author':message.sent_from.username, 'text':message.message})
+        return render_template('messages.html', form=form, messages=messages)
+    except Exception as e:
+        print(e)
+        return redirect(url_for('dialogues'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -37,12 +71,17 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful')
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
     
 
